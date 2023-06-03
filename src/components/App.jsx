3ff4dac9e62602276c93833e6b54da4deb183a4f
Anna Grigorovich <115174523+Anna-Grigorovich@users.noreply.github.com>
2023-06-04 01:27:51 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,17 @@
 import { Route, Routes } from 'react-router-dom/dist';
 import { Layout } from './Layout/Layout';
-import Home from 'page/Home/Home';
-import Movies from 'page/Movies/Movies';
-import MovieDetails from 'page/MovieDetails/MovieDetails';
-import Cast from './Cast/Cast';
-import Reviews from './Reviews/Reviews';
+// import Home from 'page/Home/Home';
+// import Movies from 'page/Movies/Movies';
+// import MovieDetails from 'page/MovieDetails/MovieDetails';
+// import Cast from './Cast/Cast';
+// import Reviews from './Reviews/Reviews';
+import { lazy } from 'react';
+
+const Home = lazy(() => import('page/Home/Home'));
+const Movies = lazy(() => import('page/Movies/Movies'));
+const MovieDetails = lazy(() => import('page/MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
 
 export const App = () => {
   return (
