eb1cdaa199aceb0d890cfffbb147f1206056f0f0
Anna Grigorovich <115174523+Anna-Grigorovich@users.noreply.github.com>
2023-06-04 13:41:04 +0300

    Update Movies.jsx

--- src/page/Movies/Movies.jsx
+++ src/page/Movies/Movies.jsx
@@ -1,17 +1,22 @@
 import { useState, useEffect } from 'react';
 import { searchMovies } from 'service/Api';
 import { Loader } from 'components/Loader/Loader';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useSearchParams } from 'react-router-dom';
 
 const Movies = () => {
-  const [query, setQuery] = useState('');
   const [movies, setMovies] = useState([]);
   const [loader, setLoader] = useState(false);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [query, setQuery] = useState(searchParams.get('movieName') ?? '');
+
+  const movieName = searchParams.get('movieName') ?? '';
+
   const location = useLocation();
 
   useEffect(() => {
     setLoader(true);
-    searchMovies(query).then(r => {
+    searchMovies(movieName).then(r => {
       setLoader(true);
       if (!r.results.length) {
         setLoader(false);
@@ -20,16 +25,29 @@ const Movies = () => {
       setMovies(r.results);
       setLoader(false);
     });
-  }, [query]);
+  }, [movieName]);
   const handleSearch = e => {
     e.preventDefault();
-    setQuery(e.currentTarget.elements.search.value);
+    console.log(query);
+    if (query === '') {
+      return alert('Enter movie name');
+    }
+    setSearchParams({ movieName: e.currentTarget.elements.search.value });
+    setQuery('');
     e.currentTarget.reset();
+  };
+  const handleInput = e => {
+    setQuery(e.target.value);
   };
   return (
     <div>
       <form onSubmit={handleSearch}>
-        <input type="text" name="search" placeholder="search movie" />
+        <input
+          type="text"
+          name="search"
+          placeholder="search movie"
+          onChange={handleInput}
+        />
         <button type="submit">SEARCH</button>
       </form>
       <ul>
